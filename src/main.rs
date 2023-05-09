c204546d9511c523df20ec49e1677b3baea5e184
Gustavo Meira <grnmeira@gmail.com>
2023-05-09 22:18:46 +0100

    WIP: introducing CLI

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,38 @@
-use clap::Parser;
+use clap::{Parser, Subcommand, Args, Arg, ArgGroup, ArgAction, command};
 use divoom::*;
 use webex;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short = 'i', help = "Webex Integration (Client) ID")]
-    integration_client_id: String,
-    #[arg(short = 's', help = "Webex Integration Client Secret")]
-    integration_client_secret: String,
-    #[arg(short = 'd', help = "Webex device ID")]
-    device_id: Option<String>,
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about="Run status integration with Pixoo Device")]
+    Run { 
+        #[arg(short='c', help="Webex integration client ID")]
+        integration_client_id: String,
+        #[arg(short='s', help="Webex integration secret")]
+        integration_secret_id: String,
+        #[arg(short='d', help="Webex device ID (if not provided, a new Webex device will be created)")]
+        webex_device_id: Option<String>,
+        #[arg(short='p', help="ID of Divoom Pixoo device (can be supressed if there's only a single device present in the local network)")]
+        pixoo_device_id: Option<String>
+    },
+    #[command(about="List Divoom Pixoo devices available in the local network")]
+    ListPixooDevices
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
 
-    let webex_authenticator = webex::auth::DeviceAuthenticator::new(
-        &args.integration_client_id,
-        &args.integration_client_secret,
-    );
+    let cli = Cli::parse();
 
-    let verification_token = webex_authenticator.verify().await.unwrap_or_else(|error| {
-        // TODO: webex crate needs to implement Display for Error
-        panic!("error obtaining verification token: {:#?}", error);
-    });
-
-    println!(
-        "Please access the following URL to authenticate your device: {}",
-        verification_token.verification_uri_complete
-    );
-
-    let bearer_token = webex_authenticator
-        .wait_for_authentication(&verification_token)
-        .await
-        .unwrap_or_else(|error| {
-            // TODO: webex crate needs to implement Display for Error
-            panic!("failure authenticating: {:#?}", error);
-        });
-
-    let webex_client = webex::api::Client::new(&bearer_token, None);
-    let mut event_listener = webex_client
-        .listen_to_events()
-        .await
-        .unwrap_or_else(|error| {
-            panic!("error trying to listen to Webex events: {:#?}", error);
-        });
-
-    loop {
-        let event = event_listener.next().await.unwrap();
-        println!("{:#?}", event);
-    }
+    std::process::exit(1);
 
     println!("Looking for Divoom devices...");
 
@@ -67,19 +48,57 @@ async fn main() {
 
     println!("{:?}", device);
 
-    let client =
-        PixooClient::new(device.device_private_ip.as_str()).expect("not able to connect to device");
+    panic!("hey");
 
-    let channel = client
-        .get_current_channel()
-        .await
-        .expect("error obtaining device channel");
+    // let webex_authenticator = webex::auth::DeviceAuthenticator::new(
+    //     &args.integration_client_id,
+    //     &args.integration_client_secret,
+    // );
 
-    println!("{:#?}", channel);
+    // let verification_token = webex_authenticator.verify().await.unwrap_or_else(|error| {
+    //     // TODO: webex crate needs to implement Display for Error
+    //     panic!("error obtaining verification token: {:#?}", error);
+    // });
 
-    let gif_url = "https://opengameart.org/sites/default/files/styles/medium/public/kaczuha_1.gif";
-    client
-        .play_gif_file(DivoomFileAnimationSourceType::Url, gif_url.to_string())
-        .await
-        .expect("not able to play gif");
+    // println!(
+    //     "Please access the following URL to authenticate your device: {}",
+    //     verification_token.verification_uri_complete
+    // );
+
+    // let bearer_token = webex_authenticator
+    //     .wait_for_authentication(&verification_token)
+    //     .await
+    //     .unwrap_or_else(|error| {
+    //         // TODO: webex crate needs to implement Display for Error
+    //         panic!("failure authenticating: {:#?}", error);
+    //     });
+
+    // let webex_client = webex::api::Client::new(&bearer_token, None);
+    // let mut event_listener = webex_client
+    //     .listen_to_events()
+    //     .await
+    //     .unwrap_or_else(|error| {
+    //         panic!("error trying to listen to Webex events: {:#?}", error);
+    //     });
+
+    // loop {
+    //     let event = event_listener.next().await.unwrap();
+    //     println!("{:#?}", event);
+    // }
+
+    // let client =
+    //     PixooClient::new(device.device_private_ip.as_str()).expect("not able to connect to device");
+
+    // let channel = client
+    //     .get_current_channel()
+    //     .await
+    //     .expect("error obtaining device channel");
+
+    // println!("{:#?}", channel);
+
+    // let gif_url = "https://opengameart.org/sites/default/files/styles/medium/public/kaczuha_1.gif";
+    // client
+    //     .play_gif_file(DivoomFileAnimationSourceType::Url, gif_url.to_string())
+    //     .await
+    //     .expect("not able to play gif");
 }
